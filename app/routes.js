702238a5d5127e04117fc09cc4191d2872707055
Ben <ben@benchilds.uk>
2018-10-15 19:17:42 +0100

    Improved route handling using regex expressions to handle archived

--- app/routes.js
+++ app/routes.js
@@ -3,32 +3,81 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 // Branching
-router.post('/eligibility-qts', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
 
-  let qts = req.session.data['qts']
+// Option A routes
+// ---------------
+// Using regex matching, these should work even for snapshots
 
-  if (qts === 'no') {
-    res.redirect('/ineligible')
+router.post(/eligibility-schools/, function (req, res) {
+
+  // From eligibility-qts
+  let eligible = req.session.data['qts']
+  if (eligible === 'no') {
+    res.redirect('ineligible')
   } else {
-    res.redirect('/eligibility-schools')
+    res.redirect('eligibility-schools')
   }
+
 })
 
-router.post('/eligibility-schools', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
+router.post(/eligibility-authority/, function (req, res) {
 
-  let school = req.session.data['school']
-
-  if (school === 'no') {
-    res.redirect('/ineligible')
+  // From eligibility-schools
+  let eligible = req.session.data['school']
+  if (eligible === 'no') {
+    res.redirect('ineligible')
   } else {
-    res.redirect('/eligibility-authority')
+    res.redirect('eligibility-authority')
   }
+
+})
+
+router.post(/eligibility-proof/, function (req, res) {
+
+  // From eligibility-authority
+  let eligible = req.session.data['authority']
+  if (eligible === 'no') {
+    res.redirect('ineligible')
+  } else {
+    res.redirect('eligibility-proof')
+  }
+
+})
+
+router.post(/eligibility-proof-loan/, function (req, res) {
+
+  // From eligibility-proof
+  let eligible = req.session.data['proof']
+  if (eligible === 'no') {
+    res.redirect('ineligible')
+  } else {
+    res.redirect('eligibility-proof-loan')
+  }
+
+})
+
+router.post(/eligibility-teaching/, function (req, res) {
+
+  // From eligibility-proof-loan
+  let eligible = req.session.data['proof-loan']
+  if (eligible === 'no') {
+    res.redirect('ineligible')
+  } else {
+    res.redirect('eligibility-teaching')
+  }
+
+})
+
+router.post(/about-you-trn/, function (req, res) {
+
+  // From eligibility-teaching
+  let eligible = req.session.data['teaching']
+  if (eligible === 'no') {
+    res.redirect('ineligible')
+  } else {
+    res.redirect('about-you-trn')
+  }
+
 })
 
 module.exports = router
